36f9ca149c34cabc4943dac0b47b09dedb3b221c
vrajeshsh <vrajeshshah13@gmail.com>
2023-09-16 17:21:06 -0400

    first test

--- igs-admin/pages/index.js
+++ igs-admin/pages/index.js
@@ -1,10 +1,5 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <div className=''></div>
+    <div className={'bg-red-500'}>test</div>
   )
 }
